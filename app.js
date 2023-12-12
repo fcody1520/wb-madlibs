490b1b739f2686df4cb31c959659638766a76e24
Cody Farrington <fcody1520@gmail.com>
2023-12-12 16:04:59 -0700

    Initial Commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import nunjucks from 'nunjucks';
+import sample from 'lodash.sample';
 
 const app = express();
 const port = '8000';
@@ -48,5 +49,33 @@ app.get('/hello', (req, res) => {
 // Handle the form from /hello and greet the user.
 app.get('/greet', (req, res) => {
   const name = req.query.name || 'stranger';
-  res.render('greet.html.njk', { name: name });
+  const compliment = sample(COMPLIMENTS)
+  res.render('greet.html.njk', { 
+    name: name,
+  compliment:compliment });
 });
+
+
+app.get('/game', (req, res) => {
+  if (req.query.play === 'no'){
+    res.render('goodbye.html.njk',
+    {name: req.query.name})
+  } else {
+    res.render('game.html')
+  }
+})
+
+app.get('/madlib', (req, res) => {
+  console.log(req.query);
+  const person = req.query.person;
+  const color = req.query.color;
+  const noun = req.query.noun;
+  const adj = req.query.adjective;
+
+  res.render('madlib.html.njk', {
+    person: person,
+    color: color,
+    noun: noun,
+    adj: adj
+  })
+})
